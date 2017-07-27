07ef0214b331bef6694742f9dc396cf5028a94c8
Ocelot1 <smurfing@As-MacBook-Pro.local>
2017-07-27 14:16:03 +0800

    Creating main class and removing test file

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Christopher Ingram
+ *
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
